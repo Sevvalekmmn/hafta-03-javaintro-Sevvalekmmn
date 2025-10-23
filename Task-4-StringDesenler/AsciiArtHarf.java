0f812ed444c86e438d5624e639124ba899b11d68
Sevvalekmmn <250541080@firat.edu.tr>
2025-10-23 20:40:03 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,11 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println("  SSSSS  ");
+        System.out.println(" S     S ");
+        System.out.println(" S        ");
+        System.out.println("  SSSSS  ");
+        System.out.println("       S ");
+        System.out.println(" S     S ");
+        System.out.println("  SSSSS  ");
+    }
+}
