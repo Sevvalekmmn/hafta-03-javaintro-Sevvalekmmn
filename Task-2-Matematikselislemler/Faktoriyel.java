b009efe34ddd8216474d94c99ac1781e203fb32c
Sevvalekmmn <250541080@firat.edu.tr>
2025-10-23 20:33:34 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,10 @@
+public class Faktoriyel {
+    public static void main(String[] args) {
+        // Faktöriyel hesaplama (5! = 5 × 4 × 3 × 2 × 1)
+        int sonuc = 5 * 4 * 3 * 2 * 1;
+
+        // Ara adımları göster
+        System.out.println("5! = 5 × 4 × 3 × 2 × 1");
+        System.out.println("Sonuç = " + sonuc);
+    }
+}
